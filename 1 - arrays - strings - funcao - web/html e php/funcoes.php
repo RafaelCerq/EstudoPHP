47246847c5492957acbcc678054a8bd2ac875103
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-01 13:22:37 -0300

    htmp e php

---
+++ 1 - arrays - strings - funcao - web/html e php/funcoes.php
@@ -0,0 +1,38 @@
+<?php
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . '<br>';
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
+function titularComLetrasMaiusculas(array &$conta)
+{
+    $conta['titular'] = mb_strtoupper($conta['titular']);
+}
+
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
+}
