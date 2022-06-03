b8c001682ce5e0f727ca6352f9f333825cd22f39
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-03 17:03:15 -0300

    Herança

--- 7 - Orientação a Objetos/Herança - Polimorfismo - Interfaces/banco.php
+++ 7 - Orientação a Objetos/Herança - Polimorfismo - Interfaces/banco.php
@@ -1,10 +1,12 @@
 <?php
 
 	require_once 'src/Conta.php';
+	require_once 'src/Endereco.php';
 	require_once 'src/Titular.php';
 	require_once 'src/CPF.php';
 
-	$joao = new Titular(new CPF('123.456.789-10'), 'João Silva');
+	$endereco = new Endereco('São Paulo', 'um bairro', 'minha rua', '71B');
+	$joao = new Titular(new CPF('123.456.789-10'), 'João Silva', $endereco);
 	$primeiraConta = new Conta($joao);
 	$primeiraConta->deposita(500);
 	$primeiraConta->saca(300); // isso é ok
@@ -13,10 +15,11 @@
 	echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 	echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-	$ana = new Titular(new CPF('564.123.784-65'), 'Ana');
+	$ana = new Titular(new CPF('698.549.548-10'), 'Ana', $endereco);
 	$segundaConta = new Conta($ana);
 	var_dump($segundaConta);
 
-	$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Lucas'));
+	$outroEndereco = new Endereco('A', 'b', 'c', 'D');
+	$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Lucas', $outroEndereco));
 	unset($segundaConta);
 	echo Conta::recuperaNumeroDeContas();
---
+++ 7 - Orientação a Objetos/Herança - Polimorfismo - Interfaces/src/Titular.php
@@ -0,0 +1,17 @@
+<?php
+
+	class Titular extends Pessoa
+	{
+		private $endereco;
+
+		public function __construct(CPF $cpf, string $nome, Endereco $endereco)
+		{
+			parent::__construct($nome, $cpf);
+			$this->endereco = $endereco;
+		}
+
+		public function recuperaEndereco(): Endereco
+		{
+			return $this->endereco;
+		}
+	}
