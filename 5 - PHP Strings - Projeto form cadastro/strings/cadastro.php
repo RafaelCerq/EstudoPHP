cb332e9080ab3a23a0743020ff9be6a619448a57
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-03 11:59:30 -0300

    Fazendo substituições e melhorando segurança

--- 5 - PHP Strings - Projeto form cadastro/strings/cadastro.php
+++ 5 - PHP Strings - Projeto form cadastro/strings/cadastro.php
@@ -2,7 +2,7 @@
 
 	require 'autoload.php';
 
-	$usuario = new \App\alura\csuario($_POST['nome'], $_POST['senha']);
+	$usuario = new \App\alura\usuario($_POST['nome'], $_POST['senha'], $_POST['genero']);
 	$contato = new \App\alura\contato($_POST['email'], $_POST['endereco'], $_POST['cep'], $_POST['telefone']);
 
 ?>
@@ -20,15 +20,15 @@
 
 		<div class="mx-5 my-5">
 			<h1>Cadastro feito com sucesso.</h1>
-			<p>Seguem os dados de sua conta:</p>
+			<p><?php echo htmlspecialchars($usuario->getTratamento()); ?>, seguem os dados de sua conta:</p>
 			<ul class="list-group">
-				<li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?></li class="list-group-item">
-				<li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?></li class="list-group-item">
-				<li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li class="list-group-item">
-				<li class="list-group-item">Senha: <?php echo $usuario->getSenha(); ?></li class="list-group-item">
-				<li class="list-group-item">Telefone: <?php echo $contato->getTelefone(); ?></li class="list-group-item">
-				<li class="list-group-item">Email: <?php echo $contato->getEmail(); ?></li class="list-group-item">
-				<li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep(); ?></li class="list-group-item">
+				<li class="list-group-item">Primeiro nome: <?php echo htmlspecialchars($usuario->getNome()); ?></li class="list-group-item">
+				<li class="list-group-item">Sobrenome: <?php echo htmlspecialchars($usuario->getSobrenome()); ?></li class="list-group-item">
+				<li class="list-group-item">Usuário: <?php echo htmlspecialchars($contato->getUsuario()); ?></li class="list-group-item">
+				<li class="list-group-item">Senha: <?php echo htmlspecialchars($usuario->getSenha()); ?></li class="list-group-item">
+				<li class="list-group-item">Telefone: <?php echo htmlspecialchars($contato->getTelefone()); ?></li class="list-group-item">
+				<li class="list-group-item">Email: <?php echo htmlspecialchars($contato->getEmail()); ?></li class="list-group-item">
+				<li class="list-group-item">Endereço: <?php echo htmlspecialchars($contato->getEnderecoCep()); ?></li class="list-group-item">
 			</ul>
 		</div>
 	</body>
