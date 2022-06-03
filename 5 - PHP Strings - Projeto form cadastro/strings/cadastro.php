9c757687b5e9b0db637d0f3adca2db26162e6c24
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-03 11:10:58 -0300

    Validações e removendo espaços inicial e final de uma string

--- 5 - PHP Strings - Projeto form cadastro/strings/cadastro.php
+++ 5 - PHP Strings - Projeto form cadastro/strings/cadastro.php
@@ -2,8 +2,8 @@
 
 	require 'autoload.php';
 
-	$usuario = new App\Alura\Usuario($_POST['nome']);
-	$contato = new App\Alura\Contato($_POST['email']);
+	$usuario = new \App\Alura\Usuario($_POST['nome'], $_POST['senha']);
+	$contato = new \App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['cep']);
 
 ?>
 
@@ -22,13 +22,13 @@
 			<h1>Cadastro feito com sucesso.</h1>
 			<p>Seguem os dados de sua conta:</p>
 			<ul class="list-group">
-				<li class="list-group-item">Primeiro nome:  <?php echo $usuario->getNome(); ?></li>
-				<li class="list-group-item">Sobrenome:  <?php echo $usuario->getSobrenome(); ?></li>
-				<li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li>
-				<li class="list-group-item">Senha: </li>
-				<li class="list-group-item">Telefone: </li>
-				<li class="list-group-item">Email: </li>
-				<li class="list-group-item">Endereço: </li>
+				<li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?></li class="list-group-item">
+				<li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?></li class="list-group-item">
+				<li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li class="list-group-item">
+				<li class="list-group-item">Senha: <?php echo $usuario->getSenha(); ?></li class="list-group-item">
+				<li class="list-group-item">Telefone: </li class="list-group-item">
+				<li class="list-group-item">Email: <?php echo $contato->getEmail(); ?></li class="list-group-item">
+				<li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep(); ?></li class="list-group-item">
 			</ul>
 		</div>
 	</body>
