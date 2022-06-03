e080398495a19e8ca5f2f63d6745446d2a51d7f0
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-03 12:32:59 -0300

    Upload de arquivos

---
+++ 6 - Upload de arquivos/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Upload</title>
+	</head>
+	<body>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			<label for="arquivo">Arquivo</label>
+			<input type="file" name="arquivo" id="arquivo">
+			<button>Enviar</button>
+		</form>
+	</body>
+</html>
---
+++ 6 - Upload de arquivos/upload.php
@@ -0,0 +1,5 @@
+<?php
+
+	var_dump($_FILES['arquivo']);
+	
+	var_dump(move_uploaded_file($_FILES['arquivo']['tmp_name'], __DIR__ .'/arquivo.jpg'));
