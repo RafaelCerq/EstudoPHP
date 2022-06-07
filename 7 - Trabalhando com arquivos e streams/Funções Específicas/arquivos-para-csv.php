b33c09a5d2d9a2a1c02ccf53a3dd26390d4eb505
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-07 15:48:34 -0300

    Decode e Encode

--- 7 - Trabalhando com arquivos e streams/Funções Específicas/arquivos-para-csv.php
+++ 7 - Trabalhando com arquivos e streams/Funções Específicas/arquivos-para-csv.php
@@ -6,14 +6,14 @@
 	$arquivoCsv = fopen('cursos.csv', 'w');
 
 	foreach ($meusCursos as $curso) {
-		$linha = [trim($curso), 'Sim'];
+		$linha = [trim(utf8_decode($curso)), 'Sim'];
 
 		//fwrite($arquivoCsv, implode(',', $linha));
 		fputcsv($arquivoCsv, $linha, ';');
 	}
 
 	foreach ($outrosCursos as $curso) {
-		$linha = [trim($curso), 'Não'];
+		$linha = [trim(utf8_decode($curso)), utf8_decode('Não')];
 
 		fputcsv($arquivoCsv, $linha, ';');
 	}
--- 7 - Trabalhando com arquivos e streams/Funções Específicas/spl.php
+++ 7 - Trabalhando com arquivos e streams/Funções Específicas/spl.php
@@ -6,7 +6,7 @@
 	while (!$arquivoCursos->eof()) {
 		$linha = $arquivoCursos->fgetcsv(';');
 
-		echo $linha[0] . PHP_EOL;
+		echo utf8_encode($linha[0]) . PHP_EOL;
 	}
 
 
