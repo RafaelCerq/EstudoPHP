e27e5b05da3175b38e30ba83b343630fe80bc4ef
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-08 16:52:49 -0300

    finally e tratamento de erros

---
+++ 9 - PHP Exceptions/Finally e Tratamento de erros/erros.php
@@ -0,0 +1,21 @@
+<?php
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	// criando tratamento de erros no proprio sistema
+	set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+		switch ($errno) {
+			case E_WARNING:
+				echo "Aviso: Isos é perigoso";
+				break;
+			case E_NOTICE:
+				echo "Melhor não fazer isso";
+				break;
+		}
+	});
+
+	echo $variavel;
+	echo $array[12];
+
+	echo CONSTANTE;
