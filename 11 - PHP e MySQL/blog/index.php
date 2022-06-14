83f440fa050e0e1cbd409e433ce01ad0c1a5b350
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-14 17:09:06 -0300

    PHP e HTML

---
+++ 11 - PHP e MySQL/blog/index.php
@@ -0,0 +1,34 @@
+<?php
+
+	include 'Artigo.php';
+	$artigo = new Artigo();
+	$artigos = $artigo->exibirTodos();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<title>Meu Blog</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<div id="container">
+			<h1>Meu Blog</h1>
+			<?php foreach ($artigos as $artigo) : ?>
+			<h2>
+				<a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+					<?php echo $artigo['titulo']; ?>
+				</a>
+			</h2>
+			<p>
+				<?php echo $artigo['conteudo']; ?>
+			</p>
+			<?php endforeach; ?>
+		</div>
+	</body>
+
+</html>
