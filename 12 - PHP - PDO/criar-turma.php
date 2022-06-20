3528563ad55c4dfb6de46e6b36c9d5c788f8ff57
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-06-20 16:35:54 -0300

    transações

---
+++ 12 - PHP - PDO/criar-turma.php
@@ -0,0 +1,30 @@
+<?php
+
+	use Alura\Pdo\Domain\Model\Student;
+	use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+	use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+	require_once 'vendor/autoload.php';
+
+	$connection = ConnectionCreator::createConnection();
+	$studentRepository = new PdoStudentRepository($connection);
+
+	// processos de definição da turma
+	$connection->beginTransaction();
+
+	$aStudent = new Student(
+		null,
+		'Julio Araujo',
+		new DateTimeImmutable('1985-05-01'),
+	);
+	$studentRepository->save($aStudent);
+
+	$anotherStudent = new Student(
+		null,
+		'Sergio Silva',
+		new DateTimeImmutable('1985-05-01'),
+	);
+	$studentRepository->save($anotherStudent);
+
+	$connection->commit();
+	// $connection->rollBack();
