5434fcdcd79950d86fa3c3603b6949e4532b062c
Rafael <rafaelcerq91@gmail.com>
2022-06-23 23:27:41 -0300

    Formulários e validações

---
+++ 14 - MVC/Gerenciador de Cursos/src/Controller/Persistencia.php
@@ -0,0 +1,49 @@
+<?php
+
+    namespace Alura\Cursos\Controller;
+
+    use Alura\Cursos\Entity\Curso;
+    use Alura\Cursos\Infra\EntityManagerCreator;
+
+    class Persistencia implements InterfaceControladorRequisicao
+    {
+        /**
+         * @var \Doctrine\ORM\EntityManagerInterface
+         */
+        private $entityManager;
+
+        public function __construct()
+        {
+            $this->entityManager = (new EntityManagerCreator())
+                ->getEntityManager();
+        }
+
+        public function processaRequisicao(): void
+        {
+            $descricao = filter_input(
+                INPUT_POST,
+                'descricao',
+                FILTER_SANITIZE_STRING
+            );
+
+            $curso = new Curso();
+            $curso->setDescricao($descricao);
+
+            $id = filter_input(
+                INPUT_GET,
+                'id',
+                FILTER_VALIDATE_INT
+            );
+
+            if (!is_null($id) && $id !== false) {
+                $curso->setId($id);
+                $this->entityManager->merge($curso);
+            } else {
+                $this->entityManager->persist($curso);
+            }
+
+            $this->entityManager->flush();
+
+            header('Location: /listar-cursos', true, 302);
+        }
+    }
