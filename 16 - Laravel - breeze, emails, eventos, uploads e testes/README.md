46a6bf840d5ce4936963dea8e81655c0acba473d
Rafael <rafaelcerq91@gmail.com>
2022-07-06 00:04:30 -0300

    projeto controle de series com login utilizando breeze

---
+++ 16 - Laravel - breeze, emails, eventos, uploads e testes/README.md
@@ -0,0 +1,57 @@
+# Projeto em PHP 8.^ e Laravel 9.^
+
+# Habilitar fileinfo para criar projeto via composer
+- Localizar o arquivo php.ini no local de instalação do PHP
+- localizar dentro do arquivo o item extension=fileinfo e retirar o comentário
+
+# Criando Projeto Laravel com composer
+- composer create-project laravel/laravel controle-series
+
+# Obs. Caso utilize este projeto, execute:
+- composer update
+
+# Server PHP Artisan (executar na raiz do projeto)
+- php artisan serve
+
+# Server PHP Artisan (com host e porta)
+- php artisan serve --host=0.0.0.0 --port=8000
+
+
+# Em caso de 500 - Server Error abra a pasta raiz do projeto e execute:
+- copy .env.example .env
+- php artisan key:generate
+
+
+# Criar controller por linha de comando
+- php artisan make:controller nomeController
+
+# Instalar componentes node (laravel mix)
+- npm install
+- npm install bootstrap
+- npm run dev (2x)
+
+# Criar Migration
+- php artisan make:migration create_series_table
+
+# Executar Migrate
+- php artisan migrate
+
+# Zera as micrations e tabelas do banco, em seguita executa novmanete Migrate
+- php artisan migrate:fresh
+
+# Criar nova migration
+- php artisan make:migration --table=episodes add_watched_apisodes
+
+# desfazer execução de Migrate
+- php artisan migrate:rollback
+- php artisan migrate:rollback --step=5
+
+# Criar Model e migration
+- php artisan make:model Season -m
+
+# Instalar Breeze (Caso o clone o projeto, apenas execute npm install e migrate )
+- composer require laravel/breeze --dev
+- php artisan breeze:install
+- npm install
+- npm run dev
+- php artisan migrate
---
+++ 16 - Laravel - breeze, emails, eventos, uploads e testes/controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function __construct(private SeriesRepository $repository)
+    {
+        $this->middleware('auth')->except('index');
+    }
+
+    public function index(Request $request)
+    {
+        $series = Series::all();
+        $mensagemSucesso = session('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = $this->repository->add($request);
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function destroy(Series $series)
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
+    }
+}
